fa8e57ad57a6cf1383eeb4be2f848cf0f1759e78
erik1110 <oceanuheart@gmail.com>
2025-11-04 15:55:29 +0800

    feat: init Bean

--- demo/src/main/java/com/example/demo/HpPrinter.java
+++ demo/src/main/java/com/example/demo/HpPrinter.java
@@ -1,11 +1,22 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
+@Component
 public class HpPrinter implements Printer {
+
+    private int count;
+
+    @PostConstruct
+    public void init() {
+        count = 5;
+    }
 
     @Override
     public void print(String message) {
+        count--;
         System.out.println("HP Printer: " + message);
+        System.out.println("HP Printer count: " + count);
     }
 }
