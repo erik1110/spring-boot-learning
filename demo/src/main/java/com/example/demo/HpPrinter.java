65e51730dd6bcc03f9308cb6b1f485a10d2553db
erik1110 <oceanuheart@gmail.com>
2025-11-06 14:10:47 +0800

    feat: aop

--- demo/src/main/java/com/example/demo/HpPrinter.java
+++ demo/src/main/java/com/example/demo/HpPrinter.java
@@ -1,22 +1,13 @@
 package com.example.demo;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer {
 
-    @Value("${printer.count}")
-    private int count;
-
-    @Value("${printer.name}")
-    private String name;
 
     @Override
     public void print(String message) {
-        count--;
-        System.out.println(name + ": " + message);
-        System.out.println("HP Printer count: " + count);
+        System.out.println("HpPrinter: " + message);
     }
 }
