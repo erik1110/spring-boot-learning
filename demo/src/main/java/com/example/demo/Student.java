894ef95010f1107eb4c22039ede8c6e505febff3
erik1110 <oceanuheart@gmail.com>
2025-11-24 18:11:08 +0800

    feat: validation

--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -1,8 +1,23 @@
 package com.example.demo;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
 public class Student {
-    String name;
-    Integer id;
+
+    @NotBlank
+    private String name;
+
+    @Min(100)
+    @NotNull
+    private Integer id;
+
+    @NotEmpty
+    private List<String> course_list;
 
     public String getName() {
         return name;
@@ -18,5 +33,13 @@ public class Student {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public List<String> getCourse_list() {
+        return course_list;
+    }
+
+    public void setCourse_list(List<String> course_list) {
+        this.course_list = course_list;
     }
 }
--- demo/src/main/java/com/example/demo/StudentController.java
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -1,18 +1,22 @@
 package com.example.demo;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RequestMapping("/students")
 @RestController
 public class StudentController {
 
     @PostMapping("")
-    public String create(@RequestBody Student student) {
-        return "Create Student";
+    public String create(@RequestBody @Valid Student student) {
+        return "Student created successfully";
     }
 
     @GetMapping("{studentId}")
-    public String read(@PathVariable Integer studentId) {
+    public String read(@PathVariable @Min(100) Integer studentId) {
         return "Get Student";
     }
 
